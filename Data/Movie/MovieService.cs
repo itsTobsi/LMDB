4760323af05e5a2d773c30b76529b111998ce595
Tobsi <tsh40884137@gmail.com>
2023-05-24 21:22:26 +0200

    Add placeholder titles to the movie card.

--- Data/Movie/MovieService.cs
+++ Data/Movie/MovieService.cs
@@ -2,12 +2,23 @@ namespace LMDB.Data.Movie;
 
 public class MovieService
 {
+    private static Random rnd = new Random();
+    private static readonly string[] MovieTitles = new[]
+    {
+        "Fast To The Future 2", "Garfield", "Not Home Alone", "Tony And The Rubber Ducks", "Mild Hot Sauce 2", "The Space Station", "The Cool, The Bold And The Programmer", "The Navy Academy 3", "Sweet Dreams"
+    };
 
     public Task<Movie[]> GetMovieAsync()
     {
-        Movie[] movies = { new Movie() { Title = "Hello World", Description = "In a new World..." } };
-        return Task.FromResult(
+        // Movie[] movies = { new Movie() { Title = "Hello World", Description = "In a new World..." } };
+        return Task.FromResult(Enumerable.Range(1, 20).Select(index => new Movie
+        {
+            Title = MovieTitles[rnd.Next(0, 8)],
+            Description = "Desc"
+
+        }).ToArray());
+        /* return Task.FromResult(
             movies
-        );
+        );*/
     }
 }
